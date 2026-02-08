357ae0547b806ec307374d3d7ad076d6f75e080a
idaranet1 <tahhan@idara.net>
2026-02-08 22:46:49 +0300

    abdt

--- idara-tracking-integration/__manifest__.py
+++ idara-tracking-integration/__manifest__.py
@@ -1,15 +1,15 @@
 
 {
-    "name": "iDARA Tracking Integration",
-    "version": "16.0.1.0.0",
-    "category": "Fleet",
-    "summary": "Free GPS tracking integration for Odoo Fleet",
-    "author": "iDaraNet",
-    "website": "https://idara.net",
-    "license": "LGPL-3",
-    "depends": ["base", "fleet", "web"],
-    "data": ["security/ir.model.access.csv"],
-    "images": ["static/description/cover.png"],
-    "installable": True,
-    "application": False
+    'name': 'iDARA Live Tracking Map',
+    'version': '1.0',
+    'category': 'Fleet',
+    'depends': ['fleet', 'web'],
+    'data': ['views/live_map_action.xml'],
+    'assets': {
+        'web.assets_backend': [
+            'idara_tracking_integration/static/src/js/live_map.js',
+            'idara_tracking_integration/static/src/xml/live_map.xml'
+        ]
+    },
+    'installable': True
 }
