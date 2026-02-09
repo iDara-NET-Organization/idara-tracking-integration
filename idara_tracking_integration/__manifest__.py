e1161162354f228ff8524a39ecc0fd58eba919e4
idaranet1 <tahhan@idara.net>
2026-02-09 22:29:32 +0300

    abdt

--- idara_tracking_integration/__manifest__.py
+++ idara_tracking_integration/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Idara Tracking Integration',
-    'version': '2.4.0',
+    'version': '2.4.2',
     'category': 'Operations',
     'summary': 'GPS Tracking with Multi-Map Support & Timezone (Odoo 15-19)',
     'description': """
